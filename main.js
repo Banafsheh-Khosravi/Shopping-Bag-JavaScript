0becfa857fa48c52fe332d146f539e238b696f94
Bani <bani@Banis-MacBook-Pro.local>
2019-11-19 20:28:00 -0500

    fix main js

--- main.js
+++ main.js
@@ -1,30 +1,3 @@
-//-------------------------Remove Button--------------------------------
-const removeCartBtn = document.querySelectorAll(".fa-trash");
-for (i = 0; i < removeCartBtn.length; i++) {
-  var btn = removeCartBtn[i]; // which element in the loop we are in
-  btn.addEventListener("click", removeItem);
-}
-// eventlistenrt always reurn an even object inside the function that it calls
-function removeItem(event) {
-  var btnClicked = event.target;
-  btnClicked.parentElement.parentElement.remove();
-  updateTotal();
-}
-
-//--------------------------Quantity Input--------------------------------------
-const quantityInput = document.querySelectorAll(".quantity-input");
-for (i = 0; i < quantityInput.length; i++) {
-  var input = quantityInput[i];
-  input.addEventListener("change", quantityChange);
-}
-
-function quantityChange(event) {
-  var input = event.target;
-  if (isNaN(input.value) || input.value <= 0) {
-    input.value = 1;
-  }
-  updateTotal();
-}
 //----------------------------Add To bag----------------------------
 const addTobag = document.querySelectorAll(".add-to-bag-button");
 for (i = 0; i < addTobag.length; i++) {
@@ -33,15 +6,15 @@ for (i = 0; i < addTobag.length; i++) {
 }
 
 function addTobagClicked(event) {
-  var button = event.target;
-  var item = button.parentElement.parentElement;
+  var button = event.target; // The target property returns the element that triggered the event.
+  var item = button.parentElement.parentElement; //The parentElement property returns the parent element of the specified element
   var title = item.querySelectorAll(".item-title")[0].innerText;
   var price = item.querySelectorAll(".item-price")[0].innerText;
   var image = item.querySelectorAll(".item-image")[0].src;
   addItemToBag(title, price, image); //***
   updateTotal();
 }
-//-------------------ADD Row-------------
+//-------------------------Add Row----------------------------------------
 function addItemToBag(title, price, image) {
   var cartRow = document.createElement("div");
   cartRow.classList.add("cart-row");
@@ -72,6 +45,35 @@ function addItemToBag(title, price, image) {
     .querySelectorAll(".quantity-input")[0]
     .addEventListener("change", quantityChange);
 }
+
+//--------------------------Quantity Input---------------------------------
+const quantityInput = document.querySelectorAll(".quantity-input");
+for (i = 0; i < quantityInput.length; i++) {
+  var input = quantityInput[i];
+  input.addEventListener("change", quantityChange);
+}
+
+function quantityChange(event) {
+  var input = event.target;
+  if (isNaN(input.value) || input.value <= 0) {
+    input.value = 1;
+  }
+  updateTotal();
+}
+
+//-------------------------Remove Button--------------------------------
+const removeCartBtn = document.querySelectorAll(".fa-trash");
+for (i = 0; i < removeCartBtn.length; i++) {
+  var btn = removeCartBtn[i]; // which element in the loop we are in
+  btn.addEventListener("click", removeItem);
+}
+// eventlistener always returns an even object inside the function that it calls
+function removeItem(event) {
+  var btnClicked = event.target;
+  btnClicked.parentElement.parentElement.remove();
+  updateTotal();
+}
+
 //-------------------------------Update Total--------------------------
 function updateTotal() {
   var cartItem = document.querySelectorAll(".cart-items")[0]; // this returns array of element , so we want only one just 1
